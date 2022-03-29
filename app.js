6f2c65d8001538e422879e9b220ebaf1beb8a6b4
aerrobidarte <aerrobidarte@uade.edu.ar>
2022-03-29 20:00:03 -0300

    Primer Version

---
+++ app.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+
+let puerto=3030;
+let publicPath=path.resolve(__dirname,'./public');
+
+app.use(express.static(publicPath));
+
+app.listen(puerto,()=>
+console.log('Servidor Corriendo'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
+
+
+/*
+
+app.get('/babbage',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./view/babbage.html'));
+});
+
+app.get('/berners-lee',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./view/berners-lee.html'));
+});
+
+app.get('/clarke',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./view/clarke.html'));
+});
+
+app.get('/hamilton',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./view/hamilton.html'));
+});
+
+app.get('/hopper',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./view/hopper.html'));
+});
+
+app.get('/lovelace',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./view/lovelace.html'));
+});
+
+app.get('/turing',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./view/turing.html'));
+});
+*/
+app.get('/*',(req,res)=>{
+    res.send("404");
+});
